924db39276fcc04146ef4311781e32d3d265a5c0
obi <resik@xaker.ru>
2025-11-17 03:35:48 +0300

    add demo

--- tts.js
+++ tts.js
@@ -1,69 +1,218 @@
 class TTS {
-    #_speechSynthesis = window.speechSynthesis || null;
-    #_speechSynthesisUtterance = new SpeechSynthesisUtterance();
-
-    #_voiceId = -1;
+    #_speechSynthesis;
+    #_speechSynthesisUtterance;
 
     constructor() {
-        this.#_speechSynthesisUtterance.volume = localStorage.getItem('tts-volume') || 1;
-        this.#_speechSynthesisUtterance.pitch = localStorage.getItem('tts-pitch') || 1;
-        this.#_speechSynthesisUtterance.rate = localStorage.getItem('tts-rate') || 1;
+        if (typeof speechSynthesis === "undefined") {
+            throw new Error("SpeechSynthesis is not supported!");
+        }
     }
 
-    loadVoicesAsync() {
+    initializeAsync() {
+        this.#_speechSynthesis = speechSynthesis;
+        this.#_speechSynthesisUtterance = new SpeechSynthesisUtterance();
+
         return new Promise((resolve, reject) => {
-            if (this.#_speechSynthesis) {
-                let timerId, attempts = 0;
-                timerId = setInterval(() => {
-                    if (++attempts > 20) {
-                        reject();
+            let timerId, attempts = 0;
+            timerId = setInterval(() => {
+                if (++attempts > 20) {
+                    clearInterval(timerId);
+                    reject();
+                } else {
+                    if (this.getVoices().length > 0) {
                         clearInterval(timerId);
-                    } else {
-                        if (this.#_speechSynthesis.getVoices().length !== 0) {
-                            resolve();
-                            clearInterval(timerId);
-                        }
+                        resolve();
                     }
-                }, 100);
-            } else {
-                reject('SpeechSynthesis is not supported!');
-            }
+                }
+            }, 100);
         });
     }
 
-    getVoiceId() {
-        return this.#_voiceId;
+    get paused() {
+        return this.#_speechSynthesis?.paused ?? false
     }
 
-    setVoiceId(val) {
-        this.#_voiceId = val;
+    get pending() {
+        return this.#_speechSynthesis?.pending ?? false
     }
 
-    getSpeechSynthesis() {
-        return this.#_speechSynthesis;
+    get speaking() {
+        return this.#_speechSynthesis?.speaking ?? false
     }
 
-    getSpeechSynthesisUtterance() {
-        return this.#_speechSynthesisUtterance;
+    cancel() {
+        this.#_speechSynthesis?.cancel()
     }
 
-    setVolume(val) {
-        localStorage.setItem('tts-volume', this.#_speechSynthesisUtterance.volume = val);
+    getVoices() {
+        return this.#_speechSynthesis?.getVoices() ?? []
     }
 
-    setPitch(val) {
-        localStorage.setItem('tts-pitch', this.#_speechSynthesisUtterance.pitch = val);
+    pause() {
+        this.#_speechSynthesis?.pause()
     }
 
-    setRate(val) {
-        localStorage.setItem('tts-rate', this.#_speechSynthesisUtterance.rate = val);
+    resume() {
+        this.#_speechSynthesis?.resume()
     }
 
-    setVoice(val) {
-        this.#_speechSynthesisUtterance.voice = val;
+    speak() {
+        if (this.#_speechSynthesis) {
+            this.#_speechSynthesis.speak(this.#_speechSynthesisUtterance)
+        }
     }
 
-    setLang(val) {
-        this.#_speechSynthesisUtterance.lang = val;
+    onVoicesChanged(callback) {
+        this.#_speechSynthesis.addEventListener("voiceschanged", callback)
+    }
+
+    offVoicesChanged(callback) {
+        this.#_speechSynthesis.removeEventListener("voiceschanged", callback)
+    }
+
+    get lang() {
+        return this.#_speechSynthesisUtterance?.lang ?? '';
+    }
+
+    set lang(val) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.lang = val;
+        }
+    }
+
+    get pitch() {
+        return this.#_speechSynthesisUtterance?.pitch ?? 0;
+    }
+
+    set pitch(val) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.pitch = val;
+        }
+    }
+
+    get rate() {
+        return this.#_speechSynthesisUtterance?.rate ?? 0;
+    }
+
+    set rate(val) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.rate = val;
+        }
+    }
+
+    get text() {
+        return this.#_speechSynthesisUtterance?.text ?? '';
+    }
+
+    set text(val) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.text = val;
+        }
+    }
+
+    get voice() {
+        return this.#_speechSynthesisUtterance?.voice ?? null;
+    }
+
+    set voice(val) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.voice = val;
+        }
+    }
+
+    get volume() {
+        return this.#_speechSynthesisUtterance?.volume;
+    }
+
+    set volume(val) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.volume = val;
+        }
+    }
+
+    onBoundary(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.addEventListener("boundary", callback)
+        }
+    }
+
+    offBoundary(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.removeEventListener("boundary", callback)
+        }
+    }
+
+    onEnd(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.addEventListener("end", callback)
+        }
+    }
+
+    offEnd(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.removeEventListener("end", callback)
+        }
+    }
+
+    onError(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.addEventListener("error", callback)
+        }
+    }
+
+    offError(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.removeEventListener("error", callback)
+        }
+    }
+
+    onMark(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.addEventListener("mark", callback)
+        }
+    }
+
+    offMark(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.removeEventListener("mark", callback)
+        }
+    }
+
+    onPause(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.addEventListener("pause", callback)
+        }
+    }
+
+    offPause(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.removeEventListener("pause", callback)
+        }
+    }
+
+    onResume(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.addEventListener("resume", callback)
+        }
+    }
+
+    offResume(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.removeEventListener("resume", callback)
+        }
+    }
+
+    onStart(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.addEventListener("start", callback)
+        }
+    }
+
+    offStart(callback) {
+        if (this.#_speechSynthesisUtterance) {
+            this.#_speechSynthesisUtterance.removeEventListener("start", callback)
+        }
     }
 }
+
+export default TTS
